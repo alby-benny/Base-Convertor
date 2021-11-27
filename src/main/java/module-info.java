3bcf5d263570eb4b5d1e5fc58598c7372f0ab4a5
lucifer4032 <89128213+lucifer4032@users.noreply.github.com>
2021-11-27 22:58:36 +0530

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.baseconvertor {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.baseconvertor to javafx.fxml;
-    exports com.example.baseconvertor;
-}
